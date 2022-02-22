9c4a747c30c542472369ae979418bb36ffe7a78e
Amrendra Nath <amrendra.nath@zeniuseducation.com>
2022-02-22 16:45:30 +0530

    part 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.hatchways.io/assessment/students")
+      .then((response) => response.json())
+      .then((data) => setData(data.students))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log("data >>", data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item) => {
+        const avg = item.grades.reduce((prev, curr) => prev + +curr, 0) / item.grades.length;
+        return (
+          <div key={item.id}>
+            <img src={item.pic} alt="" />
+            <h2>{item.firstName + " " + item.lastName}</h2>
+            <p>
+              <span>Email: </span>
+              {item.email}
+            </p>
+            <p>
+              <span>Company: </span>
+              {item.company}
+            </p>
+            <p>
+              <span>Skill: </span>
+              {item.skill}
+            </p>
+            <p>
+              <span>Average: </span>
+              {avg + "%"}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
