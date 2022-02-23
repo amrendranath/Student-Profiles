eb73a31b3ec4530f3a1c03c7828d6df947465ff2
Amrendra Nath <cool.amrendrasrivastav@gmail.com>
2022-02-23 09:01:30 +0530

    part 4 done

--- src/components/profileCard/index.js
+++ src/components/profileCard/index.js
@@ -2,20 +2,41 @@ import React from "react";
 import "./profileCard.css";
 
 const ProfileCard = ({ profile }) => {
-  const { pic, firstName, lastName, email, company, skill, avg } = profile;
+  const { pic, firstName, lastName, email, company, skill, avg, grades } = profile;
+  const [isExpand, setIsExpand] = React.useState(false);
+
   return (
-    <div className="profile-card">
-      <img src={pic} alt="" />
-      <div className="card-info">
-        <h1 className="card-title">{firstName + " " + lastName}</h1>
-        <div className="card-content">
-          <p>Email: {email}</p>
-          <p>Company: {company}</p>
-          <p>Skill: {skill}</p>
-          <p>Average: {avg + "%"}</p>
+    <>
+      <div className="profile-card">
+        <div>
+          <img src={pic} alt="" />
+        </div>
+        <div className="card-info">
+          <div className="card-header">
+            <h1 className="card-title">{firstName + " " + lastName}</h1>
+            <button className="expand-button" onClick={() => setIsExpand((prevValue) => !prevValue)}>
+              {isExpand ? "-" : "+"}
+            </button>
+          </div>
+          <div className="card-content">
+            <p>Email: {email}</p>
+            <p>Company: {company}</p>
+            <p>Skill: {skill}</p>
+            <p>Average: {avg + "%"}</p>
+          </div>
+          {isExpand && (
+            <ul>
+              {grades.map((grade, index) => (
+                <li>
+                  {`Test ${index + 1}:`}
+                  <span>{grade + "%"}</span>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
